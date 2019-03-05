e88047152f73f19be6ebb6b31fdcc97d974b05e8
Erik Marks <rekmarks@protonmail.com>
2019-03-05 09:42:03 -0800

    add missing entity type: fallback in ABI validation

--- dist/ContractGraph.js
+++ dist/ContractGraph.js
@@ -7,7 +7,7 @@
  */function validateArtifact(b){// must have contractName and abi properties
 if(!b)throw new Error("Invalid artifact: Falsy.");if(!b.contractName)throw new Error("Invalid artifact: Falsy contract name");if(!b.abi||1>b.abi.length)throw new Error("Invalid artifact: Invalid abi");// must only have one constructor
 if(1!==b.abi.filter(function(a){return"constructor"===a.type}).length)throw new Error("Parsing failure: Invalid abi (no or multiple constructors)");// abi entities must have valid types
-b.abi.forEach(function(a){if(!["constructor","function","event"].includes(a.type))throw new Error("Parsing failure: Invalid abi entity type \""+a.type+"\" for entry: "+JSON.stringify(a))})}/**
+b.abi.forEach(function(a){if(!["constructor","function","event","fallback"].includes(a.type))throw new Error("Parsing failure: Invalid abi entity type \""+a.type+"\" for entry: "+JSON.stringify(a))})}/**
  *
  */var ContractGraph=/**
    * Parses artifact and creates graph after validating artifact and options.
@@ -20,7 +20,7 @@ b.abi.forEach(function(a){if(!["constructor","function","event"].includes(a.type
  * Parses the artifact a into the ContractGraph g.
  * @param {object} a the artifact to parse
  * @param {object} g the ContractGraph that will represent the artifact
- */exports.default=ContractGraph;function parseContract(b,a){a._gs.contractName=b.contractName,b.abi.forEach(function(b){if("event"!==b.type&&(// ignore events for now
+ */exports.default=ContractGraph;function parseContract(b,a){a._gs.contractName=b.contractName,b.abi.forEach(function(b){if(// only handle constructor and function nodes
 "constructor"===b.type||// ignore function nodes if this is a constructor-only graph
-"function"===b.type&&!a.constructorOnly())){var c=(0,_v.default)(),d={id:c,nodeType:b.type,name:b.name||"constructor",payable:b.payable};b.inputs&&(d.inputs=[],b.inputs.forEach(function(b,e){var f=(0,_v.default)();addChild(b,f,c,"input",a,e),d.inputs.push(f)})),b.outputs&&(d.outputs=[],b.outputs.forEach(function(b){var e=(0,_v.default)();addChild(b,e,c,"output",a),d.outputs.push(e)})),a.usesGraphlib()&&a._gs.Graph.setNode(c,d.name),"constructor"===b.type?a._gs.constructorNodeId=c:"function"===b.type&&(!a._gs.functionNodes&&(a._gs.functionNodes=[]),a._gs.functionNodes.push(d)),a._gs.nodes.ids[c]=d,a._gs.nodes.names[d.name]=c}})}function addChild(a,b,c,d,e){var f=5<arguments.length&&void 0!==arguments[5]?arguments[5]:null,g={childId:b,nodeType:d,parent:c,name:a.name,dataType:a.type};return null!==f&&(g.order=f),e.usesGraphlib()&&e._gs.Graph.setParent(b,c),e._gs.nodes.ids[b]=g,b}
+"function"===b.type&&!a.constructorOnly()){var c=(0,_v.default)(),d={id:c,nodeType:b.type,name:b.name||"constructor",payable:b.payable};b.inputs&&(d.inputs=[],b.inputs.forEach(function(b,e){var f=(0,_v.default)();addChild(b,f,c,"input",a,e),d.inputs.push(f)})),b.outputs&&(d.outputs=[],b.outputs.forEach(function(b){var e=(0,_v.default)();addChild(b,e,c,"output",a),d.outputs.push(e)})),a.usesGraphlib()&&a._gs.Graph.setNode(c,d.name),"constructor"===b.type?a._gs.constructorNodeId=c:"function"===b.type&&(!a._gs.functionNodes&&(a._gs.functionNodes=[]),a._gs.functionNodes.push(d)),a._gs.nodes.ids[c]=d,a._gs.nodes.names[d.name]=c}})}function addChild(a,b,c,d,e){var f=5<arguments.length&&void 0!==arguments[5]?arguments[5]:null,g={childId:b,nodeType:d,parent:c,name:a.name,dataType:a.type};return null!==f&&(g.order=f),e.usesGraphlib()&&e._gs.Graph.setParent(b,c),e._gs.nodes.ids[b]=g,b}
 //# sourceMappingURL=ContractGraph.js.map
